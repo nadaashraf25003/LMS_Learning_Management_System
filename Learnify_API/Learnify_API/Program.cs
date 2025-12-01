25fb4b45ebd0db28d52b22a0fb98003f76d55e65
Nada Ashraf <nadanadaashraf25@gmail.com>
2025-12-01 12:33:06 +0200

    Update Program.cs

--- Learnify_API/Learnify_API/Program.cs
+++ Learnify_API/Learnify_API/Program.cs
@@ -20,199 +20,136 @@ namespace Learnify_API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllers()
-                .AddJsonOptions(opts =>
+// Controllers
+builder.Services.AddControllers()
+    .AddJsonOptions(opts =>
+    {
+        opts.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    });
+
+// Single DbContext registration
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("conString"),
+        sql => sql.EnableRetryOnFailure())
+);
+
+// Services
+builder.Services.AddScoped<StudentService>();
+builder.Services.AddScoped<InstructorService>();
+builder.Services.AddScoped<AdminService>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<ProfileService>();
+builder.Services.AddScoped<CourseService>();
+builder.Services.AddScoped<LessonService>();
+builder.Services.AddScoped<DashboardService>();
+builder.Services.AddScoped<QuizService>();
+builder.Services.AddScoped<QuestionService>();
+builder.Services.AddScoped<UserSettingsService>();
+builder.Services.AddScoped<CheckoutService>();
+builder.Services.AddScoped<EmailService>();
+builder.Services.AddSingleton<IEmailSender, EmailSender>();
+builder.Services.AddScoped<INotificationService, NotificationService>();
+
+// Identity
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequiredLength = 8;
+    options.SignIn.RequireConfirmedAccount = true;
+})
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+// JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ClockSkew = TimeSpan.Zero,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]!))
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme.",
+        Name = "Authorization",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
             {
-                opts.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
-                //opts.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
-                //opts.JsonSerializerOptions.MaxDepth = 64; // optional, default is 32
-            });
-            //builder.Services.AddDbContext<AppDbContext>(option =>
-            //    option.UseSqlServer(builder.Configuration.GetConnectionString("conString")));
-
-
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("conString"),
-                    sql => sql.EnableRetryOnFailure())
-            );
-
-
-            //builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDbContext>();
-            builder.Services.AddTransient<FeedbackService>();
-            //  Add CORS Policy
-            builder.Services.AddCors(options =>
-             {
-                 options.AddPolicy("AllowAll",
-                     policy =>
-                     {
-                         policy
-                             .AllowAnyOrigin()
-                             .AllowAnyHeader()
-                             .AllowAnyMethod();
-                     });
-             });
-
-            // Add services for Swagger
-            builder.Services.AddScoped<StudentService>();
-            //builder.Services.AddScoped<InstructorService>();
-            builder.Services.AddScoped<AdminService>();
-            builder.Services.AddScoped<AuthService>();
-            builder.Services.AddScoped<ProfileService>();
-            builder.Services.AddScoped<CourseService>();
-            builder.Services.AddScoped<LessonService>();
-            builder.Services.AddScoped<INotificationService, NotificationService>();
-            builder.Services.AddScoped<DashboardService>();
-            builder.Services.AddScoped<QuizService>();
-            builder.Services.AddScoped<QuestionService>();
-            builder.Services.AddScoped<UserSettingsService>();
-            // Register EmailService
-            builder.Services.AddScoped<EmailService>();
-            builder.Services.AddSingleton<IEmailSender, EmailSender>();
-
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddScoped<CheckoutService>();
-            builder.Services.AddScoped<StudentService>();
-            builder.Services.AddScoped<AdminService>();
-            builder.Services.AddScoped<InstructorService>();
-            builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-            // --------------------------------- Identity --------------------------------------------
-            builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
-            {
-                options.Password.RequireNonAlphanumeric = true;
-                options.Password.RequireUppercase = true;
-                options.Password.RequireLowercase = true;
-                options.Password.RequiredLength = 8;
-                options.SignIn.RequireConfirmedAccount = true;
-            }
-           )
-              .AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
-            // --------------------------------- Identity --------------------------------------------
-
-
-
-            // --------------------------------- JWT Configuration --------------------------------------------
-
-            builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-              .AddJwtBearer(options =>
-              {
-                  options.RequireHttpsMetadata = false;
-                  options.SaveToken = true;
-
-                  options.TokenValidationParameters = new TokenValidationParameters
-                  {
-                      ValidateIssuer = true,
-                      ValidateAudience = true,
-                      ValidateLifetime = true,
-                      ValidateIssuerSigningKey = true,
-                      ClockSkew = TimeSpan.Zero, // prevent login delay issues
-
-                      ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                      ValidAudience = builder.Configuration["Jwt:Audience"],
-                      IssuerSigningKey = new SymmetricSecurityKey(
-                          Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("Jwt:SecretKey")!))
-                  };
-              });
-
-            builder.Services.AddAuthorization();
-            builder.Services.AddScoped<EmailService>();
-
-            // --------------------------------- JWT Configuration --------------------------------------------
-
-
-            // --------------------------------- Swagger Security Setup --------------------------------------------
-
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
                 {
-                    Description = "JWT Authorization header using the Bearer scheme.",
-                    Name = "Authorization",
-                    In = Microsoft.OpenApi.Models.ParameterLocation.Header,
-                    Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
-                options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
-                {
-                    {
-                        new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-                        {
-                            Reference = new Microsoft.OpenApi.Models.OpenApiReference
-                            {
-                                Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new string[]{}
-                    }
-                      });
-            });
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins(
+            "http://localhost:5173",
+            "https://learnify-lms-depi.vercel.app"
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+    });
+});
 
+var app = builder.Build();
 
+// Middleware
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Learnify API V1");
+});
 
-            // --------------------------------- Swagger Security Setup --------------------------------------------
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-            // -------------------------------- CORS Configuration AllowFrontend --------------------------------------------
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("AllowFrontend", policy =>
-                {
-                    policy.WithOrigins(
-                        "http://localhost:5173",         // local dev
-                        "https://learnify-lms-depi.vercel.app"  // example frontend domain
-                        
-                        ) // React app URL
-                          .AllowAnyHeader()
-                          .AllowAnyMethod()
-                          .AllowCredentials(); // Important for sending cookies
-                });
-            });
-            //app.UseCors("AllowFrontend");
-            var app = builder.Build();
-            app.UseCors("AllowFrontend");
-            // -------------------------------- CORS Configuration AllowFrontend --------------------------------------------
+app.UseCors("AllowFrontend"); // Must be before Auth
+app.UseAuthentication();
+app.UseAuthorization();
 
+app.MapControllers();
 
+AppDbInitializer.SeedUsersAndRolesAsync(app).Wait();
 
-            // Configure the HTTP request pipeline.
+app.Run();
 
-            // Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Learnify API V1");
-                // Optionally expose swagger at root:
-                // c.RoutePrefix = string.Empty;
-            });
-            //}
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            // Use CORS before authorization
-            //app.UseCors("AllowReactApp");
-
-
-           
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.MapControllers();
-            AppDbInitializer.SeedUsersAndRolesAsync(app).Wait();
-            app.Run();
         }
     }
 }
